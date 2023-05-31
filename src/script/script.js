8f28edc009e91f30f7d237e4abf53e07fc8c27d6
J2K4UNM <giorgikatamadze718@gmail.com>
2023-05-31 16:49:46 +0400

    style (phone resolution)/ search input functionality

--- src/script/script.js
+++ src/script/script.js
@@ -1,33 +1,62 @@
-
-
-
-async function fetchData(){
-    try{
-        const res = await fetch('https://hplussport.com/api/products/order/price')
-        const data = await res.json()
-        console.log(data)
-
-
-        displayData(data)
+async function fetchData() {
+    try {
+      const res = await fetch('https://hplussport.com/api/products/order/price');
+      const data = await res.json();
+      console.log(data);
+      displayData(data);
     } catch (error) {
-        console.error('Error:', error);
-      }
-}
-
-function displayData(data){
-    const itemDiv = document.getElementById('items')
+      console.error('Error:', error);
+    }
+  }
+  
+  function displayData(data) {
+    const itemDiv = document.getElementById('items');
     itemDiv.innerHTML = data
-    .map((item) => `
-    <div class="itemCard">
-        <img src="${item.image}">
-        <p class="title">${item.name}</p>
-        <div>
-        <p>#${item.id}</p>
+      .map((item) => `
+        <div class="p-3 itemCard pb-4 d-flex flex-column gap-2">
+          <img src="${item.image}">
+          <p class="title">${item.name}</p>
+          <div class="id">
+            <p>#${item.id}</p>
+          </div>
+          <p class="price px-2">${item.price}$</p>
         </div>
-        <p class="price px-2">${item.price}$</p>
-    </div>
-  </div>
-    `).join("")
-}
-
-fetchData()
+      `)
+      .join('');
+  }
+  
+  function searchItems() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase(); 
+    if (searchInput) {
+      fetch('https://hplussport.com/api/products/order/price')
+        .then((res) => res.json())
+        .then((data) => {
+          const searchItem = data.filter(item => item.name.toLowerCase().includes(searchInput)); 
+  
+          const itemDiv = document.getElementById('items');
+          if (searchItem.length > 0) {
+            itemDiv.innerHTML = searchItem
+              .map((item) => `
+                <div class="p-3 itemCard pb-4 d-flex flex-column gap-2">
+                  <img src="${item.image}">
+                  <p class="title">${item.name}</p>
+                  <div class="id">
+                    <p>#${item.id}</p>
+                  </div>
+                  <p class="price px-2">${item.price}$</p>
+                </div>
+              `)
+              .join('');
+          } else {
+            displayData(data)
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+  }
+  document.getElementById('searchInput').addEventListener('input', searchItems);
+  
+  fetchData();
+  
